fb58da4b58cc7a40ccff9b5cf294b1feb937cdaf
yilmazemre61 <emreyilmz1@gmail.com>
2024-02-20 15:43:30 -0500

    Initial Commit

---
+++ Little Lemon Reservation App/Little_Lemon_Reservation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_Reservation_AppApp.swift
+//  Little Lemon Reservation App
+//
+//  Created by Emre Yilmaz on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Reservation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
